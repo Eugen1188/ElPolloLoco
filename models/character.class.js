2fe0a3097a51421078ee90cffc2e18717ac29d29
Eugen Ferchow <Eugen.Ferchow@gmx.de>
2022-05-07 00:56:48 +0200

    elpolloloco

---
+++ models/character.class.js
@@ -0,0 +1,15 @@
+class Character extends MovableObject {
+
+    y = 230;
+    height = 210;
+    width = 100;
+
+    constructor(){
+        super().loadImage('img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-21.png')
+
+    }
+
+    jump(){
+
+    }
+}
---
+++ models/world.class.js
@@ -0,0 +1,52 @@
+class World {
+
+    character = new Character();
+    enemies = [
+        new Chicken(),
+        new Chicken(),
+        new Chicken()
+    ];
+    clouds = [
+        new Cloud()
+    ];
+    backgroundObjects = [
+        new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png', 0),
+        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/1.png', 0),
+        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/1.png', 0),
+        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/1.png', 0)
+    ];
+    canvas;
+    ctx;
+
+    constructor(canvas) {
+        this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
+        this.draw();
+    }
+
+    draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+
+        this.addObjectsToMap(this.backgroundObjects);
+        this.addTopMap(this.character);
+        this.addObjectsToMap(this.clouds);
+        this.addObjectsToMap(this.enemies);
+        
+
+        let self = this;
+        requestAnimationFrame(function () {
+            self.draw();
+
+        });
+    }
+
+    addObjectsToMap(objects) {
+        objects.forEach(o => {
+            this.addTopMap(o);
+        });
+    }
+
+    addTopMap(mo) {
+        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+    }
+}
