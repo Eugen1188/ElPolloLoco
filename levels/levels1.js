3d680a1fe118a610d29c921f1f73968d69c0fcd8
Eugen Ferchow <Eugen.Ferchow@gmx.de>
2022-11-21 01:55:13 +0100

    add_walking_sound_add_endboss

--- levels/levels1.js
+++ levels/levels1.js
@@ -2,8 +2,10 @@ const level1 = new Level(
     [
         new Chicken(),
         new Chicken(),
-        new Chicken()
+        new Chicken(),
+        new Endboss()
     ],
+
     [
         new Cloud()
     ],
@@ -23,6 +25,10 @@ const level1 = new Level(
         new BackgroundObject('img/5_background/layers/air.png', 1438),
         new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 1438),
         new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 1438),
-        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 1438)
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 1438),
+        new BackgroundObject('img/5_background/layers/air.png', 2157),
+        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 2157),
+        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 2157),
+        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 2157)
     ]
 );
